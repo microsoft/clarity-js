7978a4e77c7918ad00c538240009fe822e38a995
Sam Martin <samart@microsoft.com>
2017-08-01 08:58:09 -0700

    no selenium

--- test/basicEventValidation.ts
+++ test/basicEventValidation.ts
@@ -1,10 +1,8 @@
 import * as webdriver from "selenium-webdriver";
-import * as core from "../src/core";
-import { activateCore, cleanupFixture, setupFixture } from "./utils";
 
 let username = "ender336";
 let accessKey = "fcfad206-6db2-46bc-8824-c1e576d549cf";
-let driver = new webdriver.Builder().
+/*let driver = new webdriver.Builder().
   /*withCapabilities({
     'browserName': 'chrome',
     'platform': 'Windows XP',
@@ -13,22 +11,19 @@ let driver = new webdriver.Builder().
     'accessKey': accessKey
   }).
   usingServer("http://" + username + ":" + accessKey +
-              "@ondemand.saucelabs.com:80/wd/hub").*/
+              "@ondemand.saucelabs.com:80/wd/hub").
   build();
-
+*/
 describe("Basic WebDriver Tests", () => {
 
-  beforeEach(setupFixture);
-  afterEach(cleanupFixture);
-
   it("make sure we can call sauce", (done) => {
-    core.teardown();
-    driver.get("http://www.bing.com/search?q=test");
+    /*driver.get("http://www.bing.com/search?q=test");
     driver.getTitle().then((title) => {
         console.log("title is: " + title);
     });
     driver.log("hi sam");
-    driver.quit();
+    driver.quit();*/
+    console.log("hi sam");
     done();
   });
 });
