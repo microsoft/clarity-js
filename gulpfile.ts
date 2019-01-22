da6e6364fcffd47925f8a513f41b7fff174e137e
ssivov <sivov.spb@gmail.com>
2019-01-21 19:34:47 -0800

    Travis build and test routine updates

--- gulpfile.ts
+++ gulpfile.ts
@@ -3,16 +3,13 @@ import * as gulp from "gulp";
 import * as rename from "gulp-rename";
 import * as ts from "gulp-typescript";
 import * as uglify from "gulp-uglify";
-import * as karma from "karma";
 import * as typescript from "rollup-plugin-typescript2";
 import * as rollup from "rollup-stream";
 import * as source from "vinyl-source-stream";
 
-declare const __dirname;
 const tsProject = ts.createProject("tsconfig.json");
 const bundle = "clarity.js";
 const minifiedBundle = "clarity.min.js";
-const karmaServer = karma.Server;
 
 gulp.task("uglify", () => {
   return gulp.src("build/" + bundle)
@@ -46,55 +43,9 @@ gulp.task("compile", () => {
     .pipe(gulp.dest(tsProject.config.compilerOptions.outDir));
 });
 
-gulp.task("place-fixture", () => {
-  return gulp.src("test/clarity.fixture.html")
-    .pipe(gulp.dest("build/test"));
-});
-
-gulp.task("test", (done) => {
-  new karmaServer({
-    configFile: __dirname + "/build/test/karma.conf.js",
-    singleRun: true
-  }, done).start();
-});
-
-gulp.task("test-debug", (done) => {
-  new karmaServer({
-    configFile: __dirname + "/build/test/karma.conf.js",
-    singleRun: false
-  }, done).start();
-});
-
-gulp.task("coverage", (done) => {
-  new karmaServer({
-    configFile: __dirname + "/build/test/coverage.conf.js"
-  }, done).start();
-});
-
 gulp.task("build", gulp.series(
   "clean",
   "compile",
-  "place-fixture",
   "rollup",
   "uglify"
-));
-
-// build and then run coverage
-gulp.task("bnc", gulp.series(
-  "clean",
-  "compile",
-  "place-fixture",
-  "rollup",
-  "uglify",
-  "coverage"
-));
-
-// build and then run tests
-gulp.task("bnt", gulp.series(
-  "clean",
-  "compile",
-  "place-fixture",
-  "rollup",
-  "uglify",
-  "test"
 ));
