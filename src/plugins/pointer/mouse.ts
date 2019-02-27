9074c5a7d6ae335b2ee807ce17e8b8b1d3836e0a
Sarvesh Nagpal <sarveshnagpal@gmail.com>
2019-02-27 12:17:22 -0800

    Rounding off pointer coordinates to save bytes over the wire (#121)

--- src/plugins/pointer/mouse.ts
+++ src/plugins/pointer/mouse.ts
@@ -9,8 +9,8 @@ export function transform(evt: MouseEvent): IPointerState[] {
     index: 1, /* Pointer ID */
     event: evt.type,
     pointer: "mouse",
-    x: "pageX" in evt ? evt.pageX : ("clientX" in evt ? evt["clientX"] + de.scrollLeft : null),
-    y: "pageY" in evt ? evt.pageY : ("clientY" in evt ? evt["clientY"] + de.scrollTop : null),
+    x: "pageX" in evt ? Math.round(evt.pageX) : ("clientX" in evt ? Math.round(evt["clientX"] + de.scrollLeft) : null),
+    y: "pageY" in evt ? Math.round(evt.pageY) : ("clientY" in evt ? Math.round(evt["clientY"] + de.scrollTop) : null),
     width: 1,
     height: 1,
     pressure: 1,
