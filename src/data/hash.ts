e434914a50d1a29d5a9665642534cdbb6b765f69
Sarvesh Nagpal <sarveshnagpal@gmail.com>
2019-10-08 08:24:26 -0700

    Updating hash code - in progress

--- src/data/hash.ts
+++ src/data/hash.ts
@@ -1,11 +1,16 @@
 // tslint:disable: no-bitwise
 export default function(input: string): string {
-    let value = 0;
-    for (let i = 0; i < input.length; i++) {
-        let char = input.charCodeAt(i);
-        value = ((value << 5) - value) + char;
-        value = value & value; // 32bit int conversion
+    let hash = 0;
+    let hashOne = 5381;
+    let hashTwo = hashOne;
+    for (let i = 0; i < input.length; i += 2) {
+        let charOne = input.charCodeAt(i);
+        hashOne = ((hashOne << 5) + hashOne) ^ charOne;
+        if (i + 1 < input.length) {
+            let charTwo = input.charCodeAt(i + 1);
+            hashTwo = ((hashTwo << 5) + hashTwo) ^ charTwo;
+        }
     }
-    value = value & 0xfffffff;
-    return value.toString(36);
+    hash = (hashOne + (hashTwo * 1566083941)) & 0xffffffffff;
+    return hash.toString(36);
 }
