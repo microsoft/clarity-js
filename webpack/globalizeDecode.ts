e16a8c26ae1eb68206cded5c0903f96d803dc43e
linrosales <55508792+linrosales@users.noreply.github.com>
2019-11-06 15:27:14 -0800

    wapring decode-js to expose the module (#156)
    * waring decode-js to expose the module
    
    * adding decode to the global clarity variable
    
    * exposing decode under clarity variable for decode.min.js
    
    * removing clarity intstumentation from decode

---
+++ webpack/globalizeDecode.ts
@@ -0,0 +1,11 @@
+import * as decode from "@decode/clarity";
+
+// When built with webpack for prod, compiled clarity-js bundle doesn't expose the module anywhere on the page.
+// Since we need clarity-js having the decode functionality to be available globally, we can create a wrapper
+// module that would assign clarity to window.
+if (typeof window !== "undefined") {
+    if ((window as any).clarity === undefined || (window as any).clarity === null) {
+        (window as any).clarity = {};
+    }
+    (window as any).clarity.decode = decode;
+}
