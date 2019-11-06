a7e238ace7b46de1c037aa9cae79c2f74b74ef60
linrosales <lin.rosales@hotmail.com>
2019-11-06 15:14:47 -0800

    removing clarity intstumentation from decode

--- webpack/globalizeDecode.ts
+++ webpack/globalizeDecode.ts
@@ -1,12 +1,11 @@
 import * as decode from "@decode/clarity";
-import * as clarity from "@src/clarity";
 
 // When built with webpack for prod, compiled clarity-js bundle doesn't expose the module anywhere on the page.
 // Since we need clarity-js having the decode functionality to be available globally, we can create a wrapper
 // module that would assign clarity to window.
 if (typeof window !== "undefined") {
-    if ((window as any).clarity === undefined) {
-        (window as any).clarity = clarity;
+    if ((window as any).clarity === undefined || (window as any).clarity === null) {
+        (window as any).clarity = {};
     }
     (window as any).clarity.decode = decode;
 }
