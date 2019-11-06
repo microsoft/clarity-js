67b16f7990abd8fa2377e1898f4da90571cf83d1
linrosales <lin.rosales@hotmail.com>
2019-11-06 10:21:19 -0800

    exposing decode under clarity variable for decode.min.js

--- webpack/configs/dev.ts
+++ webpack/configs/dev.ts
@@ -11,7 +11,7 @@ const DevConfig: webpack.Configuration = {
 
     entry: {
         clarity: "./webpack/globalize.ts",
-        decode: "./webpack/globalize.ts",
+        decode: "./webpack/globalizeDecode.ts",
     },
 
     output: {
--- webpack/globalize.ts
+++ webpack/globalize.ts
@@ -1,11 +1,7 @@
-import * as decode from "@decode/clarity";
 import * as clarity from "@src/clarity";
 
 // When built with webpack for prod, compiled clarity-js bundle doesn't expose the module anywhere on the page.
 // Since we need clarity-js to be available globally, we can create a wrapper module that would assign clarity to window.
 if (typeof window !== "undefined") {
-    if ((window as any).clarity === undefined) {
-        (window as any).clarity = clarity;
-    }
-    (window as any).clarity.decode = decode;
+    (window as any).clarity = clarity;
 }
