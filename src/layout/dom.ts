9992001462ffec16eac7f3c8ecb24467f5188e22
Sarvesh Nagpal <sarveshn@microsoft.com>
2019-11-18 17:04:30 -0800

    Resolving a script error in Edge

--- src/layout/dom.ts
+++ src/layout/dom.ts
@@ -184,7 +184,7 @@ export function getValue(id: number): NodeValue {
 
 export function get(node: Node): NodeValue {
     let id = getId(node);
-    return values[id];
+    return id in values ? values[id] : null;
 }
 
 export function has(node: Node): boolean {
